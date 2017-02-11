d0e27d4d96506d0783a6e46cf770f2a9473471c6
Jedsada Tiwongvorakul <jedsada@wisdomlanna.com>
2017-02-11 21:59:44 +0700

    test with Robolectric.

--- app/src/test/java/com/wisdomlanna/www/dagger2_mvp_example/ExampleUnitTest.java
+++ app/src/test/java/com/wisdomlanna/www/dagger2_mvp_example/ExampleUnitTest.java
@@ -1,17 +1,32 @@
 package com.wisdomlanna.www.dagger2_mvp_example;
 
+import android.widget.TextView;
+
+import com.wisdomlanna.www.dagger2_mvp_example.main.MainActivity;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
-import static org.junit.Assert.*;
+import static junit.framework.Assert.assertEquals;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
 public class ExampleUnitTest {
+
+    private MainActivity mainActivity;
+
+    @Before
+    public void setUp() throws Exception {
+        mainActivity = Robolectric.setupActivity(MainActivity.class);
+    }
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void shouldInjectMockStringFactory() throws Exception {
+        TextView result = (TextView) mainActivity.findViewById(R.id.tvResult);
+        assertEquals("result : 10", result.getText());
     }
 }
