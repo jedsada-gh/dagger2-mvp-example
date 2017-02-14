1e7488fd3d80648e4b3ea8ce5bc45e3322dc842c
jedsada-pc <pondthaitay@gmail.com>
2017-02-15 01:42:37 +0700

    bug test RX2.

--- app/src/test/java/com/wisdomlanna/www/dagger2_mvp_example/main/MainPresenterTest.java
+++ app/src/test/java/com/wisdomlanna/www/dagger2_mvp_example/main/MainPresenterTest.java
@@ -2,36 +2,32 @@ package com.wisdomlanna.www.dagger2_mvp_example.main;
 
 import com.wisdomlanna.www.dagger2_mvp_example.api.GitHubApi;
 import com.wisdomlanna.www.dagger2_mvp_example.base.ConnectionCallback;
+import com.wisdomlanna.www.dagger2_mvp_example.dao.UserInfoDao;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
 
-import io.reactivex.Scheduler;
-
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 
-@RunWith(MockitoJUnitRunner.class)
 public class MainPresenterTest {
 
     @Mock
     MainView view;
     @Mock
     GitHubApi api;
-    @Mock
-    ConnectionCallback callback;
-    @Mock
-    Scheduler scheduler;
     @InjectMocks
     MainInteractorImpl interactor;
+    @Mock
+    ConnectionCallback callback;
 
     private MainPresenter presenter;
+
 
     @Before
     public void setup() {
@@ -65,7 +61,18 @@ public class MainPresenterTest {
 
     @Test
     public void setOnError() throws Exception {
+        presenter.loadUserInfoGitHub("pondthaitay");
+        verify(view).hideProgressDialog();
+        callback.onServerError(anyString());
+        verify(view).showError(eq(anyString()));
+    }
 
+    @Test
+    public void setOnFailure() throws Exception {
+        presenter.loadUserInfoGitHub("pondthaitay");
+        verify(view).hideProgressDialog();
+        callback.onServerError(anyString());
+        verify(view).showError(eq(anyString()));
     }
 
     @Test
@@ -78,6 +85,11 @@ public class MainPresenterTest {
 
     @Test
     public void onSuccess() throws Exception {
-
+        presenter.loadUserInfoGitHub("pondthaitay");
+        verify(view).showProgressDialog();
+        UserInfoDao userInfoDao = new UserInfoDao();
+        userInfoDao.setCompany("dd");
+        callback.onSuccess(userInfoDao);
+        verify(view).showResultUserInfoGitHubApi(eq(userInfoDao));
     }
 }
